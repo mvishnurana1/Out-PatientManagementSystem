9f9bdba2c722195615f0b7a8f4475b24c9e4e661
Vishnu Pratap Rana <mvishnurana@gmail.com>
2019-05-24 17:13:02 +0800

    updated disease model

--- OPDManagementSystem/Models/Disease.cs
+++ OPDManagementSystem/Models/Disease.cs
@@ -4,13 +4,22 @@ namespace OPDManagementSystem.Models
 {
     public class Disease
     {
+        /// <summary>
+        /// Default constructor
+        /// </summary>
         public Disease() : this("", false){}
 
+        /// <summary>
+        /// Constructor overloaded
+        /// </summary>
+        /// <param name="title"></param>
+        /// <param name="chronic"></param>
         public Disease(string title, Boolean chronic)
         {
             MedicalIssue = title;
             Chronic = chronic; 
         }
+
 
         public String MedicalIssue { get; set; }
         public Boolean Chronic { get; set; }
